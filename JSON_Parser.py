154e30537bd1f1a1be1b93b376f73524520f64e2
rishishukla5 <rishi.shukla@innovaccer.com>
2020-01-23 16:56:21 +0530

    Writing JSON into a file

--- JSON_Parser.py
+++ JSON_Parser.py
@@ -3,4 +3,7 @@ import json
 read_file = open("quiz.json", "r", encoding="utf-8")
 data = json.load(read_file)
 read_file.close()
-print(data['quiz'])
+json.dump(data, open("output.json", "w"), indent=4, sort_keys=True)
+json_data = '{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}'
+parsed_json = json.loads(json_data)
+print(json.dumps(parsed_json, indent=4, sort_keys=True))
